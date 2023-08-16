af658ffebca250768894057ba72b4243c7511b79
Tonya Yurina <tonyabatonya@yandex.ru>
2023-08-16 20:15:10 +0300

    added class Cat and modded App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanT =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanT.getMessage());
+        System.out.println("пары бинов ссылаются на один объект?" + (beanT == bean));
+
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(firstCat.getMessage());
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(secondCat.getMessage());
+        System.out.println("пары бинов ссылаются на один объект?" + (firstCat == secondCat));
+
     }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,15 +1,16 @@
-public class HelloWorld {
- 
+public class Cat {
+
     private String message;
- 
+
     public String getMessage() {
 
         return message;
     }
- 
+
     public void setMessage(String message) {
 
         this.message = message;
     }
-     
+
+
 }
